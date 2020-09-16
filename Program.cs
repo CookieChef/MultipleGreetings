8c751c5593d764fbdef3820120d8078ed2972c1f
Sara Martinez <s_martinez_1990@outlook.com>
2020-09-15 22:39:05 -0400

    completed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace MultipleGreetings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ask the user how many times they want to be greeted
+            Console.WriteLine("How many times would you like to be greeted?");
+            var numGreets = Int32.Parse(Console.ReadLine());
+            
+            //Use while conditional to display the message the amount of time user chooses until it reaches 0. 
+            while (numGreets > 0) {
+                Console.WriteLine("Hello!");
+                numGreets --;
+            }
         }
     }
 }
